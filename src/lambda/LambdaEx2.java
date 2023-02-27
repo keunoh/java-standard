c22c8d2c94f1b67624707ba32ff66d56c0d5c1f7
Keunoh <rmsdhcjswo1@gmail.com>
2023-02-28 00:09:55 +0900

    람다식의 타입과 형변환

---
+++ src/lambda/LambdaEx2.java
@@ -0,0 +1,23 @@
+package lambda;
+
+@FunctionalInterface
+interface MyFunction2 {
+    void myMethod();    //public abstract void myMethod();
+}
+
+class LambdaEx2 {
+    public static void main(String[] args) {
+        MyFunction2 f = () -> {};   // MyFunction2 f = (MyFunction2) (() -> {});
+        Object obj = (MyFunction2) (() -> {});  //Object 타입으로 형변환이 생략됨
+        String str = ((Object)(MyFunction2) (() -> {})).toString();
+
+        System.out.println(f);
+        System.out.println(obj);
+        System.out.println(str);
+
+//        System.out.println(() -> {});   //에러. 람다식은 Object타입으로 형변환 안됨
+        System.out.println((MyFunction2) (() -> {}));
+//        System.out.println((MyFunction2) (() -> {}).toString()); //에러
+        System.out.println(((Object) (MyFunction2) (() -> {})).toString());
+    }
+}
