a0ed965a2c72b3d3b072e5ff8f627d0a37e655f0
Keunoh <rmsdhcjswo1@gmail.com>
2023-03-02 23:34:24 +0900

    예외처리하기 try-catch

---
+++ src/exception/ExceptionEx1.java
@@ -0,0 +1,17 @@
+package exception;
+
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+            try { } catch (Exception e) { }
+        } catch (Exception e) {
+            try { } catch (Exception e) { } //에러. 변수 e가 중복선언 되었다.
+        } //try-catch의 끝
+
+        try {
+
+        } catch (Exception e) {
+
+        } //try-catch의 끝
+    } //main 메서드의 끝
+}
