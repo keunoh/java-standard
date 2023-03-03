049f2d6925c820d527ae465bab6259317cadecb5
Keunoh <rmsdhcjswo1@gmail.com>
2023-03-03 21:55:33 +0900

    Exception

---
+++ src/exception/ExceptionEx10.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class ExceptionEx10 {
+    public static void main(String[] args) {
+//        throw new Exception(); //Exception을 고의로 발생시킨다.
+        //컴파일 에러
+        //Exception 클래스들이 발생할 가능성이 있는 문장들에 대해 예외처리를
+        //해주지 않으면 컴파일조차 되지 않는다.
+    }
+}
