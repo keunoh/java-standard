8a0a6c9163a134ef58fa47e9e05df42232639f8e
Keunoh <rmsdhcjswo1@gmail.com>
2023-03-05 23:44:55 +0900

    finally 블록

---
+++ src/exception/FinallyTest.java
@@ -0,0 +1,18 @@
+package exception;
+
+public class FinallyTest {
+    public static void main(String[] args) {
+        try {
+            startInstall(); //프로그램 설치에 필요한 준비를 한다.
+            copyFiles();    //파일들을 복사한다.
+            deleteTempFiles();  //프로그램 설치에 사용된 임시파일들을 삭제한다.
+        } catch (Exception e) {
+            e.printStackTrace();
+            deleteTempFiles();  //프로그램 설치에 사용된 임시파일들을 삭제한다.
+        }
+    }
+
+    static void startInstall() {}
+    static void copyFiles() {}
+    static void deleteTempFiles() {}
+}
