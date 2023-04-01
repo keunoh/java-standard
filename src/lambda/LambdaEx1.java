472a726d839052179f2ffe667a9d214ab8cae5dc
Keunoh <rmsdhcjswo1@gmail.com>
2023-04-01 19:28:22 +0900

    람다식

--- src/lambda/LambdaEx1.java
+++ src/lambda/LambdaEx1.java
@@ -1,27 +1,21 @@
 package lambda;
 
-@FunctionalInterface
-interface MyFunction {
-    void run();
-}
-
-class LambdaEx1 {
-    static void execute(MyFunction f) { //매개변수의 타입이 MyFunction인 메서드
+public class LambdaEx1 {
+    static void execute(MyFunction f) {
         f.run();
     }
 
-    static MyFunction getMyFunction() { //반환타입이 MyFunction인 메서드
+    static MyFunction getMyFunction() {
         MyFunction f = () -> System.out.println("f3.run()");
         return f;
     }
 
     public static void main(String[] args) {
-        //람다식으로 MyFunction의 run()을 구현
         MyFunction f1 = () -> System.out.println("f1.run()");
 
         MyFunction f2 = new MyFunction() {
             @Override
-            public void run() { //익명클래스로 run()을 구현
+            public void run() {
                 System.out.println("f2.run()");
             }
         };
