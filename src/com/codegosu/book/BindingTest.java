5624056a60f1254223dfba4cf4438f256240943e
keunoh <rmsdhcjswo1@gmail.com>
2023-08-18 01:02:02 +0900

    instanceof theory

---
+++ src/com/codegosu/book/BindingTest.java
@@ -0,0 +1,29 @@
+package com.codegosu.book;
+
+public class BindingTest {
+    public static void main(String[] args) {
+        Parent p = new Child();
+        Child c = new Child();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+
+    static class Parent {
+        int x = 100;
+
+        void method() {
+            System.out.println("Parent.method");
+        }
+    }
+    static class Child extends Parent {
+        int x = 200;
+
+        void method() {
+            System.out.println("Child.method");
+        }
+    }
+}
