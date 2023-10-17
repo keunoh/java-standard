39cb4dfb9d7d8d004642db3e1dbb9035ad8a6bc8
keunoh <rmsdhcjswo1@gmail.com>
2023-10-17 23:07:15 +0900

    String comparision Test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1000;
-        int b = 60;
-        int c = 60;
-        int d = 6;
-        System.out.println(a * b * c * d);
-        Runnable hello = () -> System.out.println("hello world!f");
-        hello.run();
+        String s1 = "hi";
+        String s2 = "hi";
 
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
 
+        String s3 = new String("hello");
+        String s4 = "hello";
+
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
+
+        String s5 = new String("java");
+        String s6 = new String("java");
+
+        System.out.println(s5 == s6);
+        System.out.println(s5.equals(s6));
     }
 }
