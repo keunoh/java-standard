2394b6dbd718ec0e61e1541d627ed5e7ef5abe47
keunoh <rmsdhcjswo1@gmail.com>
2023-10-26 22:55:38 +0900

    single inheritance

---
+++ src/ch7/inheritance/VCR.java
@@ -0,0 +1,21 @@
+package ch7.inheritance;
+
+public class VCR {
+    boolean power;
+    int counter = 0;
+    void power() {
+        power = !power;
+    }
+
+    void play() {
+    }
+
+    void stop() {
+    }
+
+    void rew() {
+    }
+
+    void ff() {
+    }
+}
