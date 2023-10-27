6d2df3d69f108a8b7ccde59458b82c9ab25622ea
keunoh <rmsdhcjswo1@gmail.com>
2023-10-27 23:14:19 +0900

    package && import

---
+++ src/ch7/myimport/StaticImportEx1.java
@@ -0,0 +1,14 @@
+package ch7.myimport;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.random;
+import static java.lang.System.*;
+
+public class StaticImportEx1 {
+
+    public static void main(String[] args) {
+        out.println(random());
+
+        out.println("Math.PI : " + PI);
+    }
+}
---
+++ src/ch7/mypackage/PackageTest.java
@@ -0,0 +1,7 @@
+package ch7.mypackage;
+
+public class PackageTest {
+    public static void main(String[] args) {
+        System.out.println("PackageTest.main");
+    }
+}
