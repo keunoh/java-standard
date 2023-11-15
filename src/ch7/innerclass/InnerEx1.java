888fed2267d9d4f8bcb6bbd1f5690f0d7baea273
keunoh <rmsdhcjswo1@gmail.com>
2023-11-15 22:56:44 +0900

    Inner Class

---
+++ src/ch7/innerclass/InnerEx1.java
@@ -0,0 +1,28 @@
+package ch7.innerclass;
+
+public class InnerEx1 {
+    class InstanceInner {
+        int iv = 100;
+        static int cv = 100;
+        final static int CONST = 100;
+    }
+    
+    static class StaticInner {
+        int iv = 200;
+        static int cv = 200;
+    }
+
+    void myMethod() {
+        class LocalInner {
+            int iv = 300;
+            static int cv = 300;
+            final static int CONST = 300;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("InstanceInner.CONST = " + InstanceInner.CONST);
+        System.out.println("InstanceInner.cv = " + InstanceInner.cv);
+        System.out.println("StaticInner.cv = " + StaticInner.cv);
+    }
+}
