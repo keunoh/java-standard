b719ecd22860307f7415c925a837ea27e19b3636
kaltz <rmsdhcjswo1@gmail.com>
2024-05-29 21:52:30 +0900

    theater core classes

---
+++ src/object/chapter01/theater/step01/Audience.java
@@ -0,0 +1,13 @@
+package object.chapter01.theater.step01;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
