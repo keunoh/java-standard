353bd3b830831efc19daf856b44fbd273ddac887
kaltz <rmsdhcjswo1@gmail.com>
2024-05-30 22:33:53 +0900

    refactor: add before code && enter method

--- src/object/chapter01/theater/step01/Theater.java
+++ src/object/chapter01/theater/step01/Theater.java
@@ -8,6 +8,16 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-
+        // 기존 소스
+//        if (audience.getBag().hasInvitation()) {
+//            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//            audience.getBag().setTicket(ticket);
+//        } else {
+//            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//            audience.getBag().minusAmount(ticket.getFee());
+//            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+//            audience.getBag().setTicket(ticket);
+//        }
+        ticketSeller.sellTo(audience);
     }
 }
