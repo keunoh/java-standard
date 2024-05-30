aa3a32bde1caf2e0f104543c504900df846972d2
kaltz <rmsdhcjswo1@gmail.com>
2024-05-30 22:25:09 +0900

    fix: ticketOffice code to ticketSeller method

--- src/object/chapter01/theater/step01/Theater.java
+++ src/object/chapter01/theater/step01/Theater.java
@@ -8,13 +8,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+
     }
 }
